a127374d0e029af206ac779d734de8d478c5fe98
lokeshdhakar97 <lokeshdhakar1404@gmail.com>
2025-01-11 11:09:07 +0530

    complete currenchy convert app

--- lib/cc_page.dart
+++ lib/cc_page.dart
@@ -1,8 +1,21 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
-class CurrencyConverterPage extends StatelessWidget {
+class CurrencyConverterPage extends StatefulWidget {
   const CurrencyConverterPage({super.key});
+
+  @override
+  State<CurrencyConverterPage> createState() => _CurrencyConverterPageState();
+}
+
+class _CurrencyConverterPageState extends State<CurrencyConverterPage> {
+  double amount = 0;
+  TextEditingController textEditingController = TextEditingController();
+  void convert(value) {
+    setState(() {
+      amount = double.parse(textEditingController.text) * 81;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     final border = OutlineInputBorder(
@@ -12,6 +25,7 @@ class CurrencyConverterPage extends StatelessWidget {
       ),
       borderRadius: BorderRadius.circular(40),
     );
+
     return Scaffold(
       backgroundColor: Colors.blue[50],
       appBar: AppBar(
@@ -27,20 +41,22 @@ class CurrencyConverterPage extends StatelessWidget {
         ),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text(
-              '0',
-              style: TextStyle(
-                fontSize: 36,
-                fontWeight: FontWeight.bold,
-                color: Colors.blue,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'INR ${amount != 0 ? amount.toStringAsFixed(2) : 0}',
+                style: const TextStyle(
+                  fontSize: 36,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.blue,
+                ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: TextField(
+              TextField(
+                onSubmitted: convert,
+                controller: textEditingController,
                 keyboardType: TextInputType.numberWithOptions(decimal: true),
                 style: TextStyle(
                   fontSize: 24,
@@ -56,10 +72,8 @@ class CurrencyConverterPage extends StatelessWidget {
                   focusedBorder: border,
                 ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: ElevatedButton(
+              const SizedBox(height: 16),
+              ElevatedButton(
                 onPressed: () {},
                 style: TextButton.styleFrom(
                   backgroundColor: Colors.blue,
@@ -79,8 +93,8 @@ class CurrencyConverterPage extends StatelessWidget {
                   ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
